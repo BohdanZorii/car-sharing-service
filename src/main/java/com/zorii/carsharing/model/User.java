49ded03fd45a9f0517fcdae8ad1fcfa6e765f0bf
Zorii Bohdan <bodkazorii@gmail.com>
2024-12-26 23:36:59 +0200

    add user entity with jpa support

---
+++ src/main/java/com/zorii/carsharing/model/User.java
@@ -0,0 +1,70 @@
+package com.zorii.carsharing.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.UUID;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User implements UserDetails {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+  @NotBlank(message = "Email cannot be blank")
+  @Email(message = "Email should be valid")
+  @Size(max = 100, message = "Email must not exceed 100 characters")
+  @Column(unique = true, nullable = false)
+  private String email;
+
+  @NotBlank(message = "First name cannot be blank")
+  @Size(min = 1, max = 50, message = "First name must be between 1 and 50 characters")
+  private String firstName;
+
+  @NotBlank(message = "Last name cannot be blank")
+  @Size(min = 1, max = 50, message = "Last name must be between 1 and 50 characters")
+  private String lastName;
+
+  @NotBlank(message = "Password cannot be blank")
+  @Size(min = 8, message = "Password must be at least 8 characters long")
+  private String password;
+
+  @NotNull(message = "Role cannot be null")
+  @Enumerated(EnumType.STRING)
+  private Role role = Role.CUSTOMER;
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return Collections.singletonList(new SimpleGrantedAuthority(role.name()));
+  }
+
+  @Override
+  public String getUsername() {
+    return email;
+  }
+
+  public enum Role {
+    MANAGER,
+    CUSTOMER
+  }
+}
