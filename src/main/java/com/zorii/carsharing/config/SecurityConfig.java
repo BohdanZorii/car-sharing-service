096442ca533571d3d13f8da95f1b881bcaec88ac
Zorii Bohdan <bodkazorii@gmail.com>
2024-12-28 10:35:12 +0200

    allow car listing for unauthorized users

--- src/main/java/com/zorii/carsharing/config/SecurityConfig.java
+++ src/main/java/com/zorii/carsharing/config/SecurityConfig.java
@@ -4,8 +4,10 @@ import com.zorii.carsharing.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,6 +18,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
   private final UserDetailsService userDetailsService;
@@ -31,10 +34,9 @@ public class SecurityConfig {
                 .requestMatchers("/auth/**",
                     "/error",
                     "/swagger-ui/**",
-                    "/v3/api-docs/**")
-                .permitAll()
-                .anyRequest()
-                .authenticated()
+                    "/v3/api-docs/**").permitAll()
+                .requestMatchers(HttpMethod.GET, "/cars/**").permitAll()
+                .anyRequest().authenticated()
         )
         .sessionManagement(session ->
             session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
