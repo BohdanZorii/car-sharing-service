8e3a1cac37a82bae8da5b8a0ee0885515ee1d03f
Zorii Bohdan <bodkazorii@gmail.com>
2025-01-02 10:45:18 +0200

    fix NotificationException constructor

--- src/main/java/com/zorii/carsharing/exception/NotificationException.java
+++ src/main/java/com/zorii/carsharing/exception/NotificationException.java
@@ -5,5 +5,6 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 public class NotificationException extends RuntimeException {
 
   public NotificationException(String message, TelegramApiException ex) {
+    super(message, ex);
   }
 }
