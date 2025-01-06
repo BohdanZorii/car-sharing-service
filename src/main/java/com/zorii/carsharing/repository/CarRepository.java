d4bb721928b96feaa47342d4010e3cd0755ad29c
Zorii Bohdan <bodkazorii@gmail.com>
2025-01-06 20:43:57 +0200

    add @Repository annotation where needed

--- src/main/java/com/zorii/carsharing/repository/CarRepository.java
+++ src/main/java/com/zorii/carsharing/repository/CarRepository.java
@@ -3,6 +3,8 @@ package com.zorii.carsharing.repository;
 import com.zorii.carsharing.model.Car;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CarRepository extends JpaRepository<Car, UUID> {
 }
